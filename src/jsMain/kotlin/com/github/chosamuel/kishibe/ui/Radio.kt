e8f97beeb94a731f55dd18851d17df8747d45b65
Samuel Cho <samuelcho@gmx.de>
2021-05-01 19:41:22 +0200

    btn cast as SVGElement, add css for Radio

--- src/jsMain/kotlin/com/github/chosamuel/kishibe/ui/Radio.kt
+++ src/jsMain/kotlin/com/github/chosamuel/kishibe/ui/Radio.kt
@@ -4,38 +4,41 @@ package com.github.chosamuel.kishibe.ui
 import com.github.chosamuel.kishibe.svgns
 import kotlinx.browser.document
 import kotlinx.browser.window
+import org.w3c.dom.svg.SVGElement
 
-class Radio(val numBtns:Int = 3) : BaseUI(){
+class Radio(
+    val numBtns:Int = 3,
+    var btnWidth:Double = 20.0,
+    var btnHeight:Double = 20.0,
+    var paddingWidth:Double = 30.0,
+    var topPadding:Double = (window.innerHeight - (btnHeight*2.0))
 
-    val btnWidth = 20
-    val btnHeight = 20
-    val paddingWidth = 30
+) : BaseUI(){
+
     val UIWidth = numBtns * btnWidth + (numBtns-1) * paddingWidth
     val leftPadding = (window.innerWidth - UIWidth) * 0.5
-    val topPadding = (window.innerHeight - (btnHeight*2.0))
     var mode = 0
     var onClick = {}
 
     init {
         for(i in 0 until numBtns){
-            val btn = document.createElementNS(svgns, "rect")
+            val btn = document.createElementNS(svgns, "rect") as SVGElement
             val btnx = leftPadding + (i * (btnWidth + paddingWidth))
             val id = "btn-${i+1}"
             btn.setAttribute("width", "$btnWidth")
             btn.setAttribute("id","$id")
-            if(i == 0){
-                btn.classList.add("active")
-            } else {
-                btn.classList.add("inactive")
-            }
+            val defaultState = if(i==0) "active" else "inactive"
+            btn.classList.add(defaultState)
             btn.classList.add("btn")
             btn.setAttribute("height", "$btnHeight")
             btn.setAttribute("x", "$btnx")
             btn.setAttribute("y", "$topPadding")
             btn.addEventListener("mousedown", {
+                //find current active button and deactivate
                 val activeBtn = document.querySelector(".active")
                 activeBtn?.classList?.remove("active")
                 activeBtn?.classList?.add("inactive")
+                //activate current button
                 btn.classList.remove("inactive")
                 btn.classList.add("active")
                 mode = i
@@ -43,9 +46,34 @@ class Radio(val numBtns:Int = 3) : BaseUI(){
             })
             UI.appendChild(btn)
         }
+        addCSSStyles()
     }
     fun appendToDocument(){
         document.body?.appendChild(UI)
+    }
+
+    fun addCSSStyles(){
+        val styles = """
+                
+            .active {
+                fill: rgba(211,211,211,0.8);
+            }
+            .inactive {
+                fill: rgba(211,211,211,0.3);
+            }
+            .btn {
+                transition: 0.2s;
+            }
+            .btn:hover {
+                fill: rgba(211,211,211,0.8);
+            }
+            
+            """
+
+        val styleSheet = document.createElement("style")
+        styleSheet.setAttribute("type","text/css")
+        styleSheet.innerHTML += styles
+        document.head?.appendChild(styleSheet)
     }
 
     //optional
