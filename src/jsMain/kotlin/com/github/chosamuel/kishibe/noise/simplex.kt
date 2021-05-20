5ce86ea34478cdabc8dd34178dee3d264264c296
Samuel Cho <cho.sam@icloud.com>
2021-05-20 21:32:46 +0200

    add 3d wrapping for simplex

--- src/jsMain/kotlin/com/github/chosamuel/kishibe/noise/simplex.kt
+++ src/jsMain/kotlin/com/github/chosamuel/kishibe/noise/simplex.kt
@@ -5,3 +5,7 @@ val KISHIBE_SIMPLEX = OpenSimplexNoise()
 fun simplex(x: Double, y: Double, z: Double, w: Double): Double{
     return KISHIBE_SIMPLEX.random4D(x,y,z,w)
 }
+
+fun simplex(x: Double, y: Double, z: Double): Double{
+    return KISHIBE_SIMPLEX.random3D(x,y,z)
+}
