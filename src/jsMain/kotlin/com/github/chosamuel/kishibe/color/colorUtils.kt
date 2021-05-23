adaada8872abaef498f2636315af0bbbb6d1a134
Samuel Cho <cho.sam@icloud.com>
2021-05-23 21:00:42 +0200

    refactor cosine color

--- src/jsMain/kotlin/com/github/chosamuel/kishibe/color/colorUtils.kt
+++ src/jsMain/kotlin/com/github/chosamuel/kishibe/color/colorUtils.kt
@@ -1,5 +1,7 @@
 package com.github.chosamuel.kishibe.color
 
+import com.github.chosamuel.kishibe.application.renderer.Color
+import com.github.chosamuel.kishibe.math.Vector3
 import kotlin.math.PI
 import kotlin.math.cos
 
@@ -11,6 +13,21 @@ fun cosineColor(
     d: List<Double> = listOf(0.0,0.15,0.2),
 ):List<Double> {
     val color = listOf(
+        (a[0] + b[0] * cos(PI*2 * (c[0]*t+d[0]))),
+        (a[1]+ b[1] * cos(PI*2 * (c[1]*t+d[1]))),
+        (a[2] + b[2] * cos(PI*2 * (c[2]*t+d[2])))
+    );
+    return color
+}
+
+fun cosineColor(
+    t: Double,
+    a: Vector3 = Vector3(0.5),
+    b: Vector3 = Vector3(0.5),
+    c: Vector3 = Vector3(1.0,0.7,0.4),
+    d: Vector3 = Vector3(0.0,0.15,0.2),
+):Color {
+    val color = Color(
         (a[0] + b[0] * cos(PI*2 * (c[0]*t+d[0]))),
         (a[1]+ b[1] * cos(PI*2 * (c[1]*t+d[1]))),
         (a[2] + b[2] * cos(PI*2 * (c[2]*t+d[2])))
